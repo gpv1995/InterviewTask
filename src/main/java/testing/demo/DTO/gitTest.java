e5affe4cc4951d4dbb5e44a9d1b7188fee1e7de1
GPraveenVarma <105558430+GPraveenVarma@users.noreply.github.com>
2022-12-11 11:28:52 -0500

    testing4

--- src/main/java/testing/demo/DTO/gitTest.java
+++ src/main/java/testing/demo/DTO/gitTest.java
@@ -2,11 +2,14 @@ package testing.demo.DTO;
 
 public class gitTest {
 
-    public void test1(){
+
+    public void test3()
+    {
+
 
     }
 
-    public void test1(){
+    public void test2(){
 
     }
 
